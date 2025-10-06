5022d56566676975a37292c0dd9cf660d4df982b
lisaweta <8550622@gmail.com>
2025-10-07 00:23:50 +0200

    FIX script field processing

--- nginx-lab/code/script.js
+++ nginx-lab/code/script.js
@@ -5,9 +5,15 @@ const resultDiv = document.getElementById("result");
 form.addEventListener("submit", function(e) {
     e.preventDefault();
 
-    //const formData = new FormData(this);
+    const formData = new FormData(this);
 
     let output = "<h3>🧩 подписка успешно оформлена</h3>";
+
+      for (const [name, value] of formData.entries()) {
+
+            let displayValue = value;
+            output += `<p>${name}: ${displayValue}</p>`;
+        }
 
 
     resultDiv.innerHTML = output;
